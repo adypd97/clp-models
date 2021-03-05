04030f15322225e3f711eb5972c70d7e5a06be25
adypd97 <adypd1997@gmail.com>
2021-03-06 01:38:25 +0530

    lookup tables mostly

--- exp2/parse_csv.py
+++ exp2/parse_csv.py
@@ -1,33 +1,48 @@
 #!/usr/bin/env python3
 
 import csv
+from pprint import pprint as pp
 
-sz = 0
-with open('R134a_PresSat.csv') as cf:
-    reader = csv.reader(cf, delimiter=',')
-    values = []
-    for i,row in enumerate(reader):
-        
-        if i < 2 :
-            continue
-        elif i == 2:
-            params = list(map(lambda word: word.strip(), row))[:-1]
-            PresStat = {key : [] for key in params}
-            continue
-        elif row[0] == '':
-            continue
-        #sz += 1
-        values.append(list(map(lambda val: float(val), row[:-1])))
-
-
-def col_to_row(l, i, size):
-    return [l[j][i] for j in range(size)]
+DIR_PATH = "./input/"
+TEMPFILE = DIR_PATH + 'R134a_TempSat.csv'
+PRESFILE = DIR_PATH + 'R134a_PresSat.csv'
+SUPERFILE = DIR_PATH +  'R134a_Super.csv'
 
 def transpose(l):
-    return [col_to_row(l, i, len(l)) for i in range(len(l[0]))]
-    
-values = transpose(values)
-for i, key in enumerate(PresStat.keys()):
-    PresStat[key] = values[i]
+    return [[l[j][i] for j in range(len(l))] for i in range(len(l[0]))]
+        
+def table(filename):
+    '''Construct a lookup table from csv file
 
+    Input: filename
+    Output: Lookup table 'd'
+    '''
+    with open(filename) as cf:
+        reader = csv.reader(cf, delimiter=',')
+        values = []
+        for i,row in enumerate(reader):
+            if i < 2 :
+                continue
+            elif i == 2:
+                params = list(map(lambda word: word.strip(), row))[:-1]
+                d = {key : [] for key in params}
+                continue
+            elif row[0] == '' or row == None:
+                continue
+            values.append(list(map(lambda val: float(val), row[:-1])))
+    values = transpose(values)
+    for i, key in enumerate(d.keys()):
+        d[key] = values[i]
 
+    return d
+
+def show_table(d):
+    for k,v in d.items():
+        print("Key", k)
+        print("Value", v)
+        print("-----")
+
+# R134a : Critical Pressure: 4.059 MPa, Critical Temperature 101.06°C
+if __name__ == "__main__":
+    temp = table(TEMPFILE)
+    pres = table(PRESFILE)
