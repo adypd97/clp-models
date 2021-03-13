8117c57047a27f519725d0af83537ffcd254936d
adypd97 <adypd1997@gmail.com>
2021-03-14 01:32:03 +0530

    plot, COP increases with degree of cooling, seems plausible

--- exp2/main.py
+++ exp2/main.py
@@ -25,7 +25,12 @@ def gen_T_w(T_w_i, n):
 
 def gen_I(I, n):
     import random
-    return [ random.gauss(2, 0.5) for _ in range(n) ]
+    return [ random.gauss(2, 0.1) for _ in range(n) ]
+
+def show_plot(x, y):
+    import  matplotlib.pyplot as plt
+    plt.scatter(x, y)
+    plt.show()
 
 if __name__ == "__main__":
     # generate T_w_i, T_w, V and I
@@ -34,9 +39,13 @@ if __name__ == "__main__":
     I = gen_I(2, n)             # Ampere
     T_w_i = 25                  # C, initially temp warmer
     T_w   = gen_T_w(T_w_i, n)   # C, finally cooler after 60 seconds
+    cooling = [ T_w_i - t_w for t_w in T_w ]
     comp_work = [ compressor_work(V, i) for i in I ]
     ref_work = [ refrigeration_work(T_w_i, t_w) for t_w in T_w ]
+    expCOP = exp_COP(ref_work, comp_work)
     
-    print(f'Compressor Work : {comp_work} kW')
-    print(f'Refrigerator Work : {ref_work} kW')
+    #print(f'Compressor Work : {comp_work} kW')
+    #print(f'Refrigerator Work : {ref_work} kW')
+    print(f'Cooling : {cooling} kW')
     print(f'EXP COP : {exp_COP(ref_work, comp_work)}')
+    show_plot(cooling, expCOP)
